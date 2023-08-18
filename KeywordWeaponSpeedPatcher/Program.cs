962868a3d5a56f3b02e42132a9562eba39d55c91
iAmMe27 <iAmMe27@protonmail.com>
2023-08-19 00:17:07 +0100

    fix wrong cs file commit

--- KeywordWeaponSpeedPatcher/Program.cs
+++ KeywordWeaponSpeedPatcher/Program.cs
@@ -114,6 +114,9 @@ namespace KeywordWeaponSpeedPatcher
                     patched.Data!.Speed = _settings.Value.WeapTypeClaw;
                 if (weapon.Record.HasKeyword("WeapTypeQuarterstaff", state.LinkCache))
                     patched.Data!.Speed = _settings.Value.WeapTypeQuarterstaff;
+                // iAm's addition - bow support
+                if (weapon.Record.HasKeyword("WeapTypeBow", state.LinkCache))
+                    patched.Data!.Speed = _settings.Value.WeapTypeBow;
 
                 if (Math.Abs(patched.Data!.Speed - weapon.Record.Data!.Speed) < 0.001f)
                     state.PatchMod.Remove(weapon.Record);
